dcc45c5408d4ce85532b7630342fe87b108f5a05
Joshua David Jennings <jennings.josh@gmail.com>
2022-10-02 00:51:14 -0500

    add buttons that toggle things

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,11 @@ import '../App.css';
 function App() {
   const [cars, setCars] = useState([])
   const [drivers, setDrivers] = useState([])
-  const [show, setShow] = useState(false)
+  const [showCarForm, setShowCarForm] = useState(false)
+  const [showDriverForm, setShowDriverForm] = useState(false)
+  const [showCars, setShowCars] = useState(false)
+  const [showDrivers, setShowDrivers] = useState(false)
+
   useEffect(()=> {
   //Gets cars and drivers
     fetch('http://localhost:9292/cars')
@@ -101,23 +105,58 @@ function App() {
       setDrivers(drivers.filter(d => d.id !== id))
     )
   }
+  const toggleCarForm = () => {
+    // 👇️ passed function to setState
+    setShowCarForm(current => !current);
+  };
+  const toggleDriverForm = () => {
+    // 👇️ passed function to setState
+    setShowDriverForm(current => !current);
+  };
+
+  const toggleShowCars = () => {
+    // 👇️ passed function to setState
+    setShowCars(current => !current);
+  };
+  const toggleShowDrivers = () => {
+    // 👇️ passed function to setState
+    setShowDrivers(current => !current);
+  };
+
 
 
   return (
     // create state for viewing drivers vs cars
     <div className="App">
-      {console.log(cars)}
-      {console.log(drivers)}
+      {/* {console.log(cars)}
+      {console.log(drivers)} */}
       <div className="header">
         <h2>Cars site</h2>
         <p>Here you can enter and keep track of cars you've driven and/or no longer drive anymore.</p>
       </div>
-      <div>
-        <button onClick={() => setShow(true)}>Enter a New Car</button>{show && <CreateForm postCar={postCar}/> }
-        {/* {cars.map(c => <Card car={c} patchCar={patchCar} handleDelete={handleDelete} key={`${c.id}${c.name}`}/>)} */}
-        <button onClick={() => setShow(false)}>Enter a New Driver</button><>{!show && <DriverForm postDriver={postDriver}/> }</>
-        {/* {drivers.map(d => <Driver driver={d} handleDeleteDriver={handleDeleteDriver} key={`${d.id}${d.name}`}/>)} */}
-      </div> 
+
+      <body>
+        <div>
+          <button onClick={toggleCarForm}>Enter a New Car</button>
+          {showCarForm && <CreateForm postCar={postCar}/>}
+        </div>
+        
+        <div>
+        <button onClick={toggleDriverForm}>Enter a New Driver</button>
+          {showDriverForm && <DriverForm postDriver={postDriver}/>}
+        </div> 
+
+        <div>
+          <button onClick={toggleShowCars}>Show Cars</button>
+          {showCars && <>{cars.map(c => <Card car={c} patchCar={patchCar} handleDelete={handleDelete} key={`${c.id}${c.name}`}/>)}</>}
+        </div>
+        
+        <div>
+        <button onClick={toggleShowDrivers}>Show Drivers</button>
+          {showDrivers && <>{drivers.map(d => <Driver driver={d} handleDeleteDriver={handleDeleteDriver} key={`${d.id}${d.name}`}/>)}</>}
+        </div>
+      </body>
+      
 
     </div> 
   );  
